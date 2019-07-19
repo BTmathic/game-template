d202174cab22726442f2313b04d16ae9910fe38a
Alexander Molnar <mathic@gmail.com>
2019-07-19 18:07:32 -0400

    Procedurally generate the map

--- engine.py
+++ engine.py
@@ -10,9 +10,12 @@ def main():
     screen_height = 50
     map_width = 80
     map_height = 45
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
     colours = {
-        'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_wall': libtcod.Color(169, 169, 169),
+        'dark_ground': libtcod.Color(10, 10, 10)
     }
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '&', libtcod.yellow)
@@ -22,6 +25,7 @@ def main():
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
     con = libtcod.console_new(screen_width, screen_height)
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
