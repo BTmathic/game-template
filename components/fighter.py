ee18372eccc337b26ccb7cdb65dd92f812d9bf4e
Alexander Molnar <mathic@gmail.com>
2019-07-22 23:07:10 -0400

    UI improvements - HP bar, mouse over to identify enemies

--- components/fighter.py
+++ components/fighter.py
@@ -1,3 +1,8 @@
+import tcod as libtcod
+
+from game_messages import Message
+
+
 class Fighter:
     def __init__(self, hp, defense, power):
         self.max_hp = hp
@@ -17,11 +22,11 @@ class Fighter:
         results = []
         damage = self.power - target.fighter.defense
         if damage > 0:
-            results.append({'message': '{0} attacks {1} and causes {2} damage'.format(
-                self.owner.name.capitalize(), target.name, str(damage))})
+            results.append({'message': Message('{0} attacks {1} and causes {2} damage'.format(
+                self.owner.name.capitalize(), target.name, str(damage)), libtcod.white)})
             results.extend(target.fighter.take_damage(damage))
         else:
-            results.append({'message': '{0} attacks {1} but does no damage'.format(
-                self.owner.name.capitalize(), target.name)})
+            results.append({'message': Message('{0} attacks {1} but does no damage'.format(
+                self.owner.name.capitalize(), target.name), libtcod.white)})
 
         return results
