7cdd77ca0244052e82a6f8d783653ea10c52bdcf
Alexander Molnar <mathic@gmail.com>
2019-07-22 20:47:39 -0400

    Allow entities to move diagonally

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import tcod as libtcod
+from components.fighter import Fighter
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
 from game_states import GameStates
@@ -30,7 +31,9 @@ def main():
         'light_wall': libtcod.Color(130, 110, 50),
         'light_ground': libtcod.Color(200, 180, 50)
     }
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, fighter=fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -87,8 +90,8 @@ def main():
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print('The ' + entity.name + ' ponders the meaning of its existence.')
+                if entity.ai:
+                    entity.ai.take_turn(player, fov_map, game_map, entities)
 
             game_state = GameStates.PLAYERS_TURN
 
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,5 +1,7 @@
 from random import randint
 import tcod as libtcod
+from components.ai import BasicMonster
+from components.fighter import Fighter
 from entity import Entity
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
@@ -84,9 +86,15 @@ class GameMap:
             y = randint(room.y1 + 1, room.y2 - 1)
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
-                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
+                                     fighter=fighter_component, ai=ai_component)
                 else:
-                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True,
+                                     fighter=fighter_component, ai=ai_component)
 
                 entities.append(monster)
 
